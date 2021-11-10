24b7cc11279b147e255ac7ef110ff34b4e4e6d89
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-11-11 00:08:28 +0300

    adjusted depth 1 mobs as hero need less xp

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Bestiary.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Bestiary.java
@@ -44,7 +44,7 @@ public class Bestiary {
 			case 1: default:
 				//3x rat, 1x snake
 				return new ArrayList<>(Arrays.asList(
-						Rat.class, Rat.class, Rat.class,
+						Rat.class, Rat.class,
 						Snake.class));
 			case 2:
 				//2x rat, 1x snake, 2x gnoll
