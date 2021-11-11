69ca2788c910db1da07dc742569a159b416fad5c
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-11-11 21:45:59 +0300

    buffed iron will

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/BrokenSeal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/BrokenSeal.java
@@ -70,7 +70,7 @@ public class BrokenSeal extends Item {
 	}
 
 	public int maxShield( int armTier, int armLvl ){
-		return armTier + armLvl + Dungeon.hero.pointsInTalent(Talent.IRON_WILL);
+		return armTier + armLvl + (Dungeon.hero.hasTalent(Talent.IRON_WILL) ? 1 : 0) + Dungeon.hero.pointsInTalent(Talent.IRON_WILL);
 	}
 
 	@Override
