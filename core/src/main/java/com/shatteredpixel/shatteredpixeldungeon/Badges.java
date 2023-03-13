4fc4a442da5a9b44496ab112583adf7fd6cf7956
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2023-03-14 01:14:27 +0300

    1.1: removed leftover mentions of duelist unlocking process

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Badges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Badges.java
@@ -857,20 +857,6 @@ public class Badges {
 	}
 
 	public static void validateDuelistUnlock(){
-		if (!isUnlocked(Badge.UNLOCK_DUELIST) && Dungeon.hero != null
-				&& Dungeon.hero.belongings.weapon instanceof MeleeWeapon
-				&& ((MeleeWeapon) Dungeon.hero.belongings.weapon).tier >= 2
-				&& ((MeleeWeapon) Dungeon.hero.belongings.weapon).STRReq() <= Dungeon.hero.STR()){
-
-			if (Dungeon.hero.belongings.weapon.isIdentified() &&
-					((MeleeWeapon) Dungeon.hero.belongings.weapon).STRReq() <= Dungeon.hero.STR()) {
-				displayBadge(Badge.UNLOCK_DUELIST);
-
-			} else if (!Dungeon.hero.belongings.weapon.isIdentified() &&
-					((MeleeWeapon) Dungeon.hero.belongings.weapon).STRReq(0) <= Dungeon.hero.STR()){
-				displayBadge(Badge.UNLOCK_DUELIST);
-			}
-		}
 	}
 
 	public static void validateMasteryCombo( int n ) {
