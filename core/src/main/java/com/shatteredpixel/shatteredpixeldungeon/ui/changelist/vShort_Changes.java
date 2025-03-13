1acb20e9f6c40f0582cdbb1178f3f98ae1a87bd2
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-03-14 03:19:16 +0700

    1.6: finale

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/vShort_Changes.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/vShort_Changes.java
@@ -12,7 +12,15 @@ import java.util.ArrayList;
 
 public class vShort_Changes {
     public static void addAllChanges( ArrayList<ChangeInfo> changeInfos ) {
-        ChangeInfo changes = new ChangeInfo("Short-1.5", true, "");
+        ChangeInfo changes = new ChangeInfo("Short-1.6", true, "");
+        changes.hardlight(0xCCCCCC);
+        changeInfos.add(changes);
+        changes.addButton( new ChangeButton(Icons.get(Icons.SHORTER_CHEST), "Developer Commentary",
+                "_-_ Released March 14th, 2025"));
+        changes.addButton(new ChangeButton(Icons.get(Icons.SHPX), "ShatteredPD",
+                "_-_ Ported everything from Shattered 3.0.1."));
+
+        changes = new ChangeInfo("Short-1.5", true, "");
         changes.hardlight(0xCCCCCC);
         changeInfos.add(changes);
         changes.addButton( new ChangeButton(Icons.get(Icons.SHORTER_CHEST), "Developer Commentary",
